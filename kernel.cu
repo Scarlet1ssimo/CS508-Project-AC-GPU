85fefd49df07b9422bfe55c51cc501664b6453d8
flandre <flandre@umich.edu>
2023-04-14 16:12:25 -0500

    GPU shared mem optimize

--- kernel.cu
+++ kernel.cu
@@ -1,4 +1,5 @@
 #include "kernel.hu"
+#include <cstdio>
 const int TILE_SIZE  = 32;
 const int BLOCK_SIZE = 1024;
 __global__ void ACGPUSimple(const int* tr, const char* text, int* occur, const int M, const int L, const int charSetSize) {
@@ -18,6 +19,36 @@ __global__ void ACGPUSimple(const int* tr, const char* text, int* occur, const i
 void ACGPUSimpleLaunch(const int* tr, const char* text, int* occur, const int M, const int L, const int charSetSize) {
   int blockNum = (L - 1) / (BLOCK_SIZE * TILE_SIZE) + 1;
   ACGPUSimple<<<blockNum, BLOCK_SIZE>>>(tr, text, occur, M, L, charSetSize);
+}
+const int GPUbinSize = 2048;
+__global__ void ACGPUSharedMem(const int* tr, const char* text, int* occur, const int M, const int L, const int charSetSize,
+                               const int trieNodeNumber) {
+  int idx         = blockIdx.x * blockDim.x * TILE_SIZE;
+  int threadStart = idx + threadIdx.x * TILE_SIZE;
+  int threadEnd   = threadStart + TILE_SIZE + M - 1;
+
+  int state = 0;
+  __shared__ int localOccur[GPUbinSize];
+  for (int i = threadIdx.x; i < GPUbinSize; i += blockDim.x) {
+    localOccur[i] = 0;
+  }
+  __syncthreads();
+  for (int i = threadStart; i < threadEnd && i < L; i++) {
+    state = tr[state * charSetSize + text[i]];
+    if (state < GPUbinSize) // CPU:Reorder trie node index by depth
+      atomicAdd(&localOccur[state], 1);
+    else
+      atomicAdd(&occur[state], 1);
+  }
+  __syncthreads();
+  for (int i = threadIdx.x; i < GPUbinSize; i += blockDim.x) {
+    atomicAdd(&occur[i], localOccur[i]);
+  }
+}
+void ACGPUSharedMemLaunch(const int* tr, const char* text, int* occur, const int M, const int L, const int charSetSize,
+                          const int trieNodeNumber) {
+  int blockNum = (L - 1) / (BLOCK_SIZE * TILE_SIZE) + 1;
+  ACGPUSharedMem<<<blockNum, BLOCK_SIZE>>>(tr, text, occur, M, L, charSetSize, trieNodeNumber);
 }
 /*
 0816......1917....
--- main.cu
+++ main.cu
@@ -9,19 +9,59 @@ void checkCuda(cudaError_t result, const char* file, const int line) {
     exit(-1);
   }
 }
-
+#define RED "\x1B[31m"
+#define GREEN "\x1B[32m"
+#define INFO "\x1B[34m"
+#define YELLOW "\x1B[33m"
+#define NORMAL "\033[0m"
+struct Timer {
+  cudaEvent_t start, stop;
+  Timer() {
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+  }
+  ~Timer() {
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+  }
+  void Start() {
+    cudaEventRecord(start, 0);
+  }
+  void Stop() {
+    cudaEventRecord(stop, 0);
+  }
+  void StartAndPrint(const char* msg) {
+    printf("%s...", msg);
+    Start();
+  }
+  void StopAndPrint() {
+    Stop();
+    printf(" %f ms\n" NORMAL, elapsed());
+  }
+  float elapsed() {
+    float time;
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&time, start, stop);
+    return time;
+  }
+};
 // M: pattern length (like 8 for pattern "ACGTACGT")
 // N: number of all patterns, typically 16000
 // L: text length, typically 1e6
 // Charset (4 for ACGT)
 // Kernel version
+Timer timer;
+#define TIMER_START(msg) timer.StartAndPrint(msg)
+#define TIMER_STOP() timer.StopAndPrint()
+#define SET_COLOR(color) printf(color)
+
 void eval(int M, int N, int L, int kernel_id, const int charSetSize, const char* testName) {
   // TODO: Prettify the output
   // At least output the run time
 
   // Generate random patterns and text
   printf("====================================\n");
-  printf("Start testing %s: (M=%d, N=%d, L=%d)\n", testName, M, N, L);
+  printf(YELLOW "Start testing %s: (M=%d, N=%d, L=%d)\n" NORMAL, testName, M, N, L);
   char** patterns = (char**) malloc(N * sizeof(char*));
   for (int i = 0; i < N; i++) {
     patterns[i] = (char*) malloc((M + 1) * sizeof(char));
@@ -31,6 +71,7 @@ void eval(int M, int N, int L, int kernel_id, const int charSetSize, const char*
   random_string(text, charSetSize, L);
 
   // Allocate memory on CPU for building Trie and Aho-Corasick Algorithm
+  TIMER_START("Building Trie and Aho-Corasick Automaton on CPU");
   int trieNodeBound = N * M;
   int* tr           = (int*) malloc(trieNodeBound * charSetSize * sizeof(int)); // tr[trieNodeBound][charSetSize] -> trieNodeBound;
   int* idx          = (int*) malloc(N * sizeof(int));                           // idx[N] -> trieNodeBound;
@@ -45,45 +86,63 @@ void eval(int M, int N, int L, int kernel_id, const int charSetSize, const char*
 
   // Build Aho-Corasick Automaton on CPU
   ACBuildCPU(tr, fail, postOrder, charSetSize);
+  TIMER_STOP();
 
   // Setup for GPU launch
   int *d_tr, *d_occur;
   char* d_text;
+  TIMER_START("Allocating and copying memory on GPU");
   CUDA_RUNTIME(cudaMalloc(&d_tr, trieNodeNumber * charSetSize * sizeof(int)));
   CUDA_RUNTIME(cudaMalloc(&d_occur, trieNodeNumber * sizeof(int)));
   CUDA_RUNTIME(cudaMalloc(&d_text, (L + 1) * sizeof(char)));
   CUDA_RUNTIME(cudaMemcpy(d_tr, tr, trieNodeNumber * charSetSize * sizeof(int), cudaMemcpyHostToDevice));
   CUDA_RUNTIME(cudaMemcpy(d_text, text, (L + 1) * sizeof(char), cudaMemcpyHostToDevice));
   CUDA_RUNTIME(cudaMemset(d_occur, 0, trieNodeNumber * sizeof(int)));
+  TIMER_STOP();
 
   // Run Aho-Corasick Algorithm on GPU
+  TIMER_START(INFO "Running Aho-Corasick Algorithm on GPU");
+  SET_COLOR(YELLOW);
   if (kernel_id == 0)
     ACGPUSimpleLaunch(d_tr, d_text, d_occur, M, L, charSetSize);
-  // TODO: Add more kernel launch here
-  else {
-    printf("Error: kernel_id = %d is not supported", kernel_id);
-    goto bad;
+  else if (kernel_id == 1) {
+    ACGPUSharedMemLaunch(d_tr, d_text, d_occur, M, L, charSetSize, trieNodeNumber);
+  } else {
+    printf(RED "Error: kernel_id = %d is not supported", kernel_id);
+    TIMER_STOP();
+    // goto bad;
   }
+  CUDA_RUNTIME(cudaDeviceSynchronize());
+  TIMER_STOP();
 
   // Copy result back to CPU
-  CUDA_RUNTIME(cudaDeviceSynchronize());
+  TIMER_START("Copying result back to CPU");
   CUDA_RUNTIME(cudaMemcpy(occur_gpu, d_occur, trieNodeNumber * sizeof(int), cudaMemcpyDeviceToHost));
+  TIMER_STOP();
   // End of GPU
 
   // Run Aho-Corasick Algorithm on CPU
+  TIMER_START(INFO "Running Aho-Corasick Algorithm on CPU");
   ACCPU(tr, text, occur_cpu, L, charSetSize);
+  TIMER_STOP();
   // Post processing to get the final result
+  TIMER_START("Postprocessing on CPU");
   ACPostCPU(occur_cpu, fail, postOrder, trieNodeNumber - 1);
   ACPostCPU(occur_gpu, fail, postOrder, trieNodeNumber - 1);
+  TIMER_STOP();
 
   // Verification against CPU result
+  TIMER_START("Verifying result");
   for (int i = 0; i < N; i++)
     if (occur_cpu[idx[i]] != occur_gpu[idx[i]]) {
-      printf("Error at %d: %d %d\n", i, occur_cpu[idx[i]], occur_gpu[idx[i]]);
+      printf(RED "Error at %d: %d %d\n", i, occur_cpu[idx[i]], occur_gpu[idx[i]]);
+      TIMER_STOP();
       goto bad;
     }
-  printf("Pass\n");
+  TIMER_STOP();
+  printf(GREEN "Pass\n" NORMAL);
 bad:
+
   printf("====================================\n");
   CUDA_RUNTIME(cudaFree(d_tr));
   CUDA_RUNTIME(cudaFree(d_occur));
@@ -101,8 +160,11 @@ bad:
 }
 int main() {
   srand(940012978);
-  eval(8, 16000, 1e6, 0, 4, "ACSimple");
-  eval(8, 16000, 1e7, 0, 4, "ACSimple");
-  // eval(8, 16000, 1e8, 0, 4, "ACSimple");
+  // eval(8, 16000, 1e6, 0, 4, "ACSimple");
+  // eval(8, 16000, 1e7, 0, 4, "ACSimple");
+  eval(8, 16000, 1e8, 0, 4, "ACSimple");
+  // eval(8, 16000, 1e8, 1, 4, "ACSharedMem");
+  // eval(8, 160, 1e8, 0, 4, "ACSimple");
+  eval(8, 16000, 1e8, 1, 4, "ACSharedMem");
   // eval(8, 16000, 1e9, 0, 4, "ACSimple");
 }
